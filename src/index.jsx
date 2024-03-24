13ac424cadc4aa04dc539549dc743bb1781152d3
MHendriF <hendrifebriansyah28@gmail.com>
2024-03-24 08:36:38 +0700

    register input

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,13 @@
-import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import ContactApp from './components/ContactApp';
+import ContactApp from "./components/ContactApp";
 
 // styling
-import './styles/style.css';
- 
-const root = createRoot(document.getElementById('root'));
+import "./styles/style.css";
+
+const root = createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
-     <ContactApp />
-   </BrowserRouter>
-  );
+  <BrowserRouter>
+    <ContactApp />
+  </BrowserRouter>
+);
